bbf01afb434ecc553bc74413d8d94238e428a24b
Mohamed Mohy <mohamedmohy9192@gmail.com>
2018-04-23 19:54:09 +0200

    modify extractBoxOfficeMovie method to extract the trakt id and move year. Add convertMovieList to the class.

--- app/src/main/java/com/era/www/movietracker/utilities/TraktTvAPIJsonUtils.java
+++ app/src/main/java/com/era/www/movietracker/utilities/TraktTvAPIJsonUtils.java
@@ -1,7 +1,11 @@
 package com.era.www.movietracker.utilities;
 
 
-import com.era.www.movietracker.model.BoxOfficeMovie;
+import android.content.ContentValues;
+import android.text.TextUtils;
+
+import com.era.www.movietracker.data.MoviesContract.BoxOfficeEntry;
+import com.era.www.movietracker.model.Movie;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -12,35 +16,94 @@ import java.util.List;
 
 public class TraktTvAPIJsonUtils {
 
-    public static List<BoxOfficeMovie> BoxOfficeJsonStr(String boxOfficeJsonStr) throws JSONException {
+    public static final String SUP_MOVIE_KEY = "movie";
 
-        final String MOVIE_INFO_KEY = "movie";
+    public static final String MOVIE_REVENUE_KEY = "revenue";
+    public static final String MOVIE_TITLE_KEY = "title";
+    public static final String MOVIE_YEAR_KEY = "year";
 
-        final String MOVIE_TITLE_KEY = "title";
+    public static final String MOVIE_IDS_KEY = "ids";
+    public static final String TRAKT_ID_KEY = "trakt";
 
-        final String MOVIE_REVENUE_KEY = "revenue";
+    public static List<Movie> extractBoxOfficeMovieFromJson(String boxOfficeJsonStr) {
 
-        JSONArray boxOfficeMoviesArray = new JSONArray(boxOfficeJsonStr);
-
-        List<BoxOfficeMovie> boxOfficeMovies = new ArrayList<>();
-
-        byte rank = 1;
-
-        for (int i = 0; i < boxOfficeMoviesArray.length(); i++) {
-
-            JSONObject movieObject = boxOfficeMoviesArray.getJSONObject(i);
-
-            int movieRevenue = movieObject.getInt(MOVIE_REVENUE_KEY);
-
-            JSONObject movieInfo = movieObject.getJSONObject(MOVIE_INFO_KEY);
-
-            String movieTitle = movieInfo.getString(MOVIE_TITLE_KEY);
-
-            BoxOfficeMovie boxOfficeMovie = new BoxOfficeMovie(movieTitle, movieRevenue, rank++);
-
-            boxOfficeMovies.add(boxOfficeMovie);
+        if (TextUtils.isEmpty(boxOfficeJsonStr)) {
+            return null;
         }
+        List<Movie> boxOfficeMovieList = new ArrayList<>();
 
-        return boxOfficeMovies;
+        int movieRevenue = 0;
+        String movieTitle = null;
+        int movieYear = 0;
+        int movieTraktId = 0;
+        int movieRank = 1;
+
+        try {
+            JSONArray boxOfficeMoviesArray = new JSONArray(boxOfficeJsonStr);
+
+            for (int i = 0; i < boxOfficeMoviesArray.length(); i++) {
+
+                JSONObject mainMovieObject = boxOfficeMoviesArray.getJSONObject(i);
+
+                movieRank = movieRank + 1;
+
+                if (mainMovieObject.has(MOVIE_REVENUE_KEY)) {
+                    movieRevenue = mainMovieObject.optInt(MOVIE_REVENUE_KEY);
+                }
+
+                if (mainMovieObject.has(SUP_MOVIE_KEY)) {
+                    JSONObject supMovieObject = mainMovieObject.getJSONObject(SUP_MOVIE_KEY);
+
+                    if (supMovieObject.has(MOVIE_TITLE_KEY)) {
+                        movieTitle = supMovieObject.optString(MOVIE_TITLE_KEY);
+                    }
+                    if (supMovieObject.has(MOVIE_YEAR_KEY)) {
+                        movieYear = mainMovieObject.optInt(MOVIE_YEAR_KEY);
+                    }
+
+                    if (supMovieObject.has(MOVIE_IDS_KEY)) {
+                        JSONObject movieIdsObject = supMovieObject.optJSONObject(MOVIE_IDS_KEY);
+
+                        if (movieIdsObject.has(TRAKT_ID_KEY)) {
+                            movieTraktId = movieIdsObject.optInt(TRAKT_ID_KEY);
+                        }
+                    }
+                }
+                Movie boxOfficeMovie =
+                        new Movie(movieRevenue, movieTitle, movieYear, movieRank, movieTraktId);
+                boxOfficeMovieList.add(boxOfficeMovie);
+            }
+            return boxOfficeMovieList;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * covert movie list to contentValues array contain movie data.
+     *
+     * @param movieList movie list contain extracted  movies data from the json response.
+     * @return array of contentValues contain movies data to insert into database.
+     */
+    public static ContentValues[] geContentValuesArrayFromMovieList(List<Movie> movieList) {
+        if (movieList == null) {
+            return null;
+        }
+        ContentValues[] contentValuesArray = new ContentValues[movieList.size()];
+
+        for (int i = 0; i < movieList.size(); i++) {
+            Movie movie = movieList.get(i);
+
+            ContentValues movieContentValues = new ContentValues();
+            movieContentValues.put(BoxOfficeEntry.COLUMN_MOVIE_REVENUE, movie.getRevenue());
+            movieContentValues.put(BoxOfficeEntry.COLUMN_MOVIE_TITLE, movie.getTitle());
+            movieContentValues.put(BoxOfficeEntry.COLUMN_MOVIE_YEAR, movie.getYear());
+            movieContentValues.put(BoxOfficeEntry.COLUMN_TRAKT_ID, movie.getMovieTraktId());
+            movieContentValues.put(BoxOfficeEntry.COLUMN_MOVIE_RANK, movie.getRank());
+
+            contentValuesArray[i] = movieContentValues;
+        }
+        return contentValuesArray;
     }
 }
