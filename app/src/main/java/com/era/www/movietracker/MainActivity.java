19c62ae6fde57bd005473774db7d845a51f05c8f
Mohamed <mohamedmohy9192@gmail.com>
2020-02-19 20:11:55 +0200

    Add a navigation drawer with Nav component

--- app/src/main/java/com/era/www/movietracker/MainActivity.java
+++ app/src/main/java/com/era/www/movietracker/MainActivity.java
@@ -1,171 +1,71 @@
 package com.era.www.movietracker;
 
-import android.content.Intent;
-import android.content.res.Configuration;
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
-import androidx.fragment.app.Fragment;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.era.www.movietracker.movies.MoviesFragment;
-import com.era.www.movietracker.settings.SettingsActivity;
-
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
-
-    private static final String TAG = "tag";
-    private DrawerLayout drawerLayout;
-
-    // index to identify current nav menu item
-    private int navItemIndex = 0;
-
-    private String[] activityTitles;
-
-    private ActionBarDrawerToggle mActionBarDrawerToggle;
-
-    private Toolbar toolbar;
-
-    private NavigationView navigationView;
+public class MainActivity extends AppCompatActivity  {
 
 
-    private static final String TAG_MOVIES = "movies_fragment";
-    private static final String TAG_TV_FRAGMENT = "tv_fragment";
-    private static final String TAG_PEOPLE_FRAGMENT = "people_fragment";
-    private static String CURRENT_TAG = TAG_MOVIES;
+    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
 
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
 
-        mActionBarDrawerToggle = new ActionBarDrawerToggle(
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        NavController navController = Navigation.findNavController(
                 this,
-                drawerLayout,
-                toolbar,
-                R.string.navigation_drawer_open,
-                R.string.navigation_drawer_close);
-        drawerLayout.addDrawerListener(mActionBarDrawerToggle);
+                R.id.nav_host_fragment);
 
-        navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_movies, R.id.nav_tv_show, R.id.nav_people)
+                .setDrawerLayout(drawerLayout)
+                .build();
 
-        //Gets the header view at the specified position.
-        View navHeader = navigationView.getHeaderView(0);
-        loadNavHeaderInfo(navHeader);
+        NavigationUI.setupActionBarWithNavController(
+                this,
+                navController,
+                mAppBarConfiguration);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        NavigationUI.setupWithNavController(navigationView, navController);
 
+//        Gets the header view at the specified position.
+//        View navHeader = navigationView.getHeaderView(0);
+//        loadNavHeaderInfo(navHeader);
 
-        if (savedInstanceState == null) {
-            navItemIndex = 0;
-            CURRENT_TAG = TAG_MOVIES;
-            loadHomeFragment();
-        }
-    }
-
-    private ActionBarDrawerToggle setupDrawerToggle() {
-
-        return mActionBarDrawerToggle;
-    }
-
-
-
-    private void loadNavHeaderInfo(View navHeader) {
-        ImageView profileImage = (ImageView) navHeader.findViewById(R.id.header_profile_image);
-        TextView userName = (TextView) navHeader.findViewById(R.id.nav_header_name);
-        userName.setText("Mohamed mohamed");
-        TextView userEmail = (TextView) navHeader.findViewById(R.id.nav_header_email);
-        userEmail.setText("mohamed_mohamed@yahoo.com");
-    }
-
-    private void loadHomeFragment() {
-        navigationView.getMenu().getItem(navItemIndex).setChecked(true);
-        // load toolbar titles from string resources
-//        String[] activityTitles = getResources().getStringArray(R.array.nav_item_activity_titles);
-        // set toolbar title
-//        setTitle(activityTitles[navItemIndex]);
-
-        // if user select the current navigation menu again, don't do anything
-        // just close the navigation drawer
-        if (getSupportFragmentManager().findFragmentByTag(CURRENT_TAG) != null) {
-            drawerLayout.closeDrawers();
-        }
-
-        Fragment fragment = getSelectedFragment();
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.frame_content, fragment, CURRENT_TAG)
-                .commit();
-
-        drawerLayout.closeDrawers();
-
-
-    }
-
-    private Fragment getSelectedFragment() {
-        switch (navItemIndex) {
-            case 0:
-                return new MoviesFragment();
-            case 1:
-                return new TvShowFragment();
-            case 2:
-                return new PeopleFragment();
-            default:
-                return new MoviesFragment();
-        }
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawers();
-        }
-
-        if (navItemIndex != 0) {
-            navItemIndex = 0;
-            CURRENT_TAG = TAG_MOVIES;
-            loadHomeFragment();
-            return;
-        }
-
-        super.onBackPressed();
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
 
     }
 
     @Override
-    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        mActionBarDrawerToggle.syncState();
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        mActionBarDrawerToggle.onConfigurationChanged(newConfig);
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 
     @Override
@@ -183,43 +83,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-
+        if (id == R.id.action_settings) {
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.nav_movies:
-                navItemIndex = 0;
-                CURRENT_TAG = TAG_MOVIES;
-                break;
-            case R.id.nav_tv_show:
-                navItemIndex = 1;
-                CURRENT_TAG = TAG_TV_FRAGMENT;
-                break;
-            case R.id.nav_people:
-                navItemIndex = 2;
-                CURRENT_TAG = TAG_PEOPLE_FRAGMENT;
-                break;
-            case R.id.nav_settings:
-                Intent openSettingsActivity = new Intent(
-                        MainActivity.this,
-                        SettingsActivity.class);
-                startActivity(openSettingsActivity);
-                drawerLayout.closeDrawers();
-                return true;
-            default:
-                navItemIndex = 0;
-                CURRENT_TAG = TAG_MOVIES;
-        }
-        item.setChecked(true);
-        loadHomeFragment();
-        drawerLayout.closeDrawers();
-        return true;
-    }
 }
