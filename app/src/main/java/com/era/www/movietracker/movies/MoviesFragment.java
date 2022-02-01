934dcf0e82294b76555ae7fb448e73cb2fe8856e
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2022-02-01 12:00:16 +0200

    Revert "Replace ViewPager with ViewPager 2"
    This reverts commit 8da76f7a

--- app/src/main/java/com/era/www/movietracker/movies/MoviesFragment.java
+++ app/src/main/java/com/era/www/movietracker/movies/MoviesFragment.java
@@ -1,21 +1,19 @@
 package com.era.www.movietracker.movies;
 
 import android.os.Bundle;
-
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.tabs.TabLayout;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.viewpager2.adapter.FragmentStateAdapter;
-import androidx.viewpager2.widget.ViewPager2;
-
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.era.www.movietracker.R;
-import com.google.android.material.tabs.TabLayoutMediator;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,12 +21,6 @@ import java.util.List;
 public class MoviesFragment extends Fragment {
 
     private static final String LOG_TAG = MoviesFragment.class.getSimpleName();
-
-    private ViewPagerAdapter mViewPagerAdapter;
-    private ViewPager2 mViewPager2;
-    private TabLayout mTabLayout;
-
-    private final String[] mFragmentsName = {"Box Office", "Coming Soon", "Popular"};
 
     public MoviesFragment() {
         // Required empty public constructor
@@ -40,49 +32,105 @@ public class MoviesFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_movies, container, false);
 
+        AppBarLayout appBarLayout = (AppBarLayout) getActivity().findViewById(R.id.app_bar);
+
+        TabLayout tabLayout = (TabLayout) getActivity().findViewById(R.id.main_tab_layout);
+
+        setTabLayoutCollapseMode(tabLayout);
+
+        Toolbar toolbar = (Toolbar) getActivity().findViewById(R.id.toolbar);
+        setToolbarCollapseMode(toolbar);
+
+        //Set the AppBar expand to true to wrap the CollapsingToolbarLayout height.
+        appBarLayout.setExpanded(true);
+
+        ViewPager viewPager = (ViewPager) view.findViewById(R.id.view_pager);
+
+        setUpViewPager(viewPager);
+
+        tabLayout.setupWithViewPager(viewPager);
+
         return view;
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    /**
+     *
+     * @param tabLayout reference to TabLayout widget in app_bar_main.xml file.
+     * Use this method to change the TabLayout Collapse Mode to PIN if it's none
+     * and it's Visibility to Visible.
+     */
+    private void setTabLayoutCollapseMode(TabLayout tabLayout){
+        if (tabLayout.getVisibility() == View.GONE) {
+            tabLayout.setVisibility(View.VISIBLE);
+        }
 
-        mViewPagerAdapter = new ViewPagerAdapter(this);
-        mViewPager2 = view.findViewById(R.id.view_pager);
+        CollapsingToolbarLayout.LayoutParams params =
+                (CollapsingToolbarLayout.LayoutParams) tabLayout.getLayoutParams();
 
-        mViewPagerAdapter.addFragment(new BoxOfficeFragment());
-        mViewPagerAdapter.addFragment(new ComingSoonFragment());
-        mViewPagerAdapter.addFragment(new PopularFragment());
-
-        mViewPager2.setAdapter(mViewPagerAdapter);
-
-        mTabLayout = view.findViewById(R.id.main_tab_layout);
-        new TabLayoutMediator(mTabLayout, mViewPager2,
-                (tab, position) -> tab.setText(mFragmentsName[position])).attach();
+        if (params.getCollapseMode() ==
+                CollapsingToolbarLayout.LayoutParams.COLLAPSE_MODE_OFF) {
+            params.setCollapseMode(CollapsingToolbarLayout.LayoutParams.COLLAPSE_MODE_PIN);
+            tabLayout.setLayoutParams(params);
+        }
     }
 
-    private class ViewPagerAdapter extends FragmentStateAdapter {
+    /**
+     *
+     * @param toolbar reference to Toolbar widget in app_bar_main.xml file.
+     * Use this method to change the Toolbar Collapse Mode to PARALLAX if it's PIN
+     */
+    private void setToolbarCollapseMode(Toolbar toolbar){
+        CollapsingToolbarLayout.LayoutParams toolbarParams =
+                (CollapsingToolbarLayout.LayoutParams) toolbar.getLayoutParams();
+
+        if (toolbarParams.getCollapseMode() ==
+                CollapsingToolbarLayout.LayoutParams.COLLAPSE_MODE_PIN) {
+            toolbarParams.setCollapseMode(
+                    CollapsingToolbarLayout.LayoutParams.COLLAPSE_MODE_PARALLAX);
+            toolbar.setLayoutParams(toolbarParams);
+        }
+    }
+
+    private void setUpViewPager(ViewPager viewPager) {
+        //getChildFragmentManager
+        //Return a private FragmentManager for placing and managing Fragments inside of this Fragment.
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getChildFragmentManager());
+        viewPagerAdapter.addFragment(new BoxOfficeFragment(), "Box Office");
+        viewPagerAdapter.addFragment(new ComingSoonFragment(), "Coming Soon");
+        viewPagerAdapter.addFragment(new PopularFragment(), "Popular");
+        viewPager.setAdapter(viewPagerAdapter);
+    }
+
+    private class ViewPagerAdapter extends FragmentPagerAdapter {
 
         private final List<Fragment> fragmentList = new ArrayList<>();
+        private final List<String> fragmentTitlesList = new ArrayList<>();
 
-        public ViewPagerAdapter(@NonNull Fragment fragment) {
-            super(fragment);
+
+        ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
         }
 
-
-        void addFragment(Fragment fragment) {
-            fragmentList.add(fragment);
-        }
-
-        @NonNull
         @Override
-        public Fragment createFragment(int position) {
+        public Fragment getItem(int position) {
             return fragmentList.get(position);
         }
 
         @Override
-        public int getItemCount() {
+        public int getCount() {
             return fragmentList.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragmentTitlesList.get(position);
+        }
+
+
+
+        void addFragment(Fragment fragment, String title) {
+            fragmentList.add(fragment);
+            fragmentTitlesList.add(title);
         }
     }
 }
